571683a51276720b007f37849570cae97c877435
vicxstar <98316400+vicxstar@users.noreply.github.com>
2022-10-11 16:19:45 +0100

    Add files via upload
    Hello World file in NetBeans via GitHub

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+package com.mthree.helloworld;
+
+/**
+ *@author Victoria Ayoade
+ *email: vickiayoade@gmail.com
+ *date: 11/10/2022
+ *purpose: My first Hello World program in an IDE
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Vic!");
+    }
+}
