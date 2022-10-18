3548b75fe03f3982c314db4d01e0569589f8d8ec
vicxstar <vickiayoade@gmail.com>
2022-10-18 11:38:11 +0100

    completed the lucky sevens and interest calc exercises

--- NetBeansProjects/Week 1/ProjectProgramming/src/main/java/com/sg/foundations/exercises/interestcalculator/InterestCalculator.java
+++ NetBeansProjects/Week 1/ProjectProgramming/src/main/java/com/sg/foundations/exercises/interestcalculator/InterestCalculator.java
@@ -1,5 +1,7 @@
 package com.sg.foundations.exercises.interestcalculator;
 
+import java.util.Scanner;
+
 /**
  *
  * @author vic
@@ -12,8 +14,69 @@ public class InterestCalculator {
     
     public static void main(String[] args) {
         
+        //declare the variables:
+        String sInvest, sYears, sInterestRate;
+        double invest, interestRate, sum;
+        int years;
+        double earned = 0, end = 0;
+        
+        //decalre the scanner:
+        Scanner userIn = new Scanner(System.in);
+        
+        //print a statement asking for user input:
+        //how much to invest:
+        System.out.println("How much do you want to invest?: ");
+        //add input into the 'invest' variable:
+        sInvest = userIn.nextLine();
+        //convert input:
+        invest = Double.parseDouble(sInvest);
+        
+        //how many investment years:
+        System.out.println("How many years are investing?: ");
+        //add input into the 'years' variable:
+        sYears = userIn.nextLine();
+        //convert input:
+        years = Integer.parseInt(sYears);
+        
+        //what is the interest rate?:
+        System.out.println("What is the annual interest rate % growth?: ");
+        //add input into the 'interestRate' variable:
+        sInterestRate = userIn.nextLine();
+        //convert input:
+        interestRate = Double.parseDouble(sInterestRate)/100;
         
         
+        //print:
+        System.out.println("Calculating... ");
+        
+        //call the method and add the variables for it to function on:
+        compIntCalc(invest, years, interestRate, 4);
+        
+        
+    }
+    
+    //the calculation method:
+    //the varaibles to calculate on:
+    public static void compIntCalc(double principle, int years, double interest, int comp) {
+        
+        //create a 'for' loop to iterate through until the conditions have been met
+        for (int i = 1; i <= years; i++) {
+            
+            //using the 'Math' method:
+            double total = Math.floor((principle * (Math.pow(1 + (interest/comp), comp * 1))) 
+                    * 100) / 100;
+            //double total = principle * (Math.pow((1 + interest / 100), year)) - pri;
+            double compInt = Math.floor((total - principle) * 100) / 100;
+            
+            //print statements:
+            System.out.println("Year " + i + ": ");
+            System.out.println("Began with $" + principle);
+            System.out.println("Earned $" + compInt);
+            System.out.println("Ended with $" + total);  
+            System.out.println("");
+            
+            Math.floor(((principle += compInt) * 100) / 100);
+        }  
     }
     
 }
--- NetBeansProjects/Week 1/ProjectProgramming/src/main/java/com/sg/foundations/exercises/luckysevens/LuckySevens.java
+++ NetBeansProjects/Week 1/ProjectProgramming/src/main/java/com/sg/foundations/exercises/luckysevens/LuckySevens.java
@@ -1,13 +1,73 @@
-package com.sg.foundations.flowcontrol.luckysevens;
+package com.sg.foundations.exercises.luckysevens;
+
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
  * @author vic
  * email: vickiayoade@gmail.com
- * //////////////////////////////date: 13/10/2022
+ * date: 18/10/2022
  * purpose: Lucky Sevens
  * 
  */
 public class LuckySevens {
     
-}
+    public static void main(String[] args) {
+        
+        //decalre the scanner:
+        Scanner userIn = new Scanner(System.in);
+        
+        //print the statements and declare the variables:
+        System.out.println("How many dollars do you have?: ");
+        String sDollars = userIn.nextLine();
+        //convert the string into an integer and store it in a new variable:
+        int dollars = Integer.parseInt(sDollars);
+        System.out.println();
+        
+        //declare the randomiser:
+        Random dice = new Random();
+        
+        //declare the variables:
+        int rolls = 0, maxRolls = 0, moneys = dollars;
+        
+        //create a loop to iterate until the value in the 'dollars' variable is finished:
+        while(dollars > 0){
+            
+            //decalre a random number between 1 and 6 for the dice:
+            int roll1 = dice.nextInt(7);
+            int roll2 = dice.nextInt(7);
+            
+            //use an 'if' statement to iterate through the random numbers:
+            if (roll1 + roll2 == 7) {
+                int wins = 4;
+                
+                //increase the value of 'dollars' with the value of 'wins':
+                dollars += wins;
+                
+                //count the number of rolls:
+                rolls ++;
+                
+                //use an 'if' statement to check the amount left:
+                if (wins + moneys >= moneys) {
+                    
+                    //increase the value of 'moneys' with the value of 'wins':
+                    moneys += wins;
+                    maxRolls = rolls;
+                }
+                
+                //otherwise, count the amount of time the user lost:
+            } else {
+                int lost = 1;
+                dollars -= lost;
+                rolls ++;
+            }
+        }
+        
+        //print:
+        System.out.println("You were broke after: " + rolls + " rolls");
+        System.out.println("You should have quit after " + maxRolls + " when you had $" + moneys);
+            
+        }
+    
+    }
